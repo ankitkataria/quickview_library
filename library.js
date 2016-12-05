5c46d636504ca470a5a61bb6eecc52e00fdb4a80
ankitkataria <ankitkataria28@gmail.com>
2016-12-05 13:52:22 +0530

    added zoom in when the qview opens

--- library.js
+++ library.js
@@ -10,18 +10,26 @@ function showQview(n){
 	}
 	document.getElementsByClassName("outfocus")[0].style.zIndex=99;
     document.getElementsByClassName("outfocus")[0].style.opacity=1;
+	
+
+    ele[n].style.height="50%";
 	ele[n].style.opacity=1;
+
 }
 
-function hideQview(){
+function hideQview(n){
 	var i=0;
 	document.getElementsByClassName("qview")[0].style.zIndex=-1; 
 	var ele=$(".qviewele");
+//	ele[n].style.height="100%";
+	//ele[n].style.opacity=0;
 	for(i=0;i<ele.length;i++){
+	   ele[i].style.height="100%";
 	   ele[i].style.opacity=0;
 	}
 	document.getElementsByClassName("outfocus")[0].style.zIndex=-1;
     document.getElementsByClassName("outfocus")[0].style.opacity=0;
+	
 	$(".container").fadeTo(1000,1);
 
     
